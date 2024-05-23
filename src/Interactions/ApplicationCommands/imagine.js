d7f14c3beccf61add5029ebe5462d325ceb37d4e
0xEzSwim <enzo.chantereault@efrei.net>
2024-05-23 11:38:52 +0200

    Squashed commit of the following:
    commit 7506d8206f40d1ee50b829770bd7e79492553208
    Author: 0xEzSwim <enzo.chantereault@efrei.net>
    Date:   Thu May 23 11:38:18 2024 +0200
    
        feat(text): UX/UI text edit
    
    commit 65163a553a799717a7f0c246652f08e715f50414
    Author: 0xEzSwim <enzo.chantereault@efrei.net>
    Date:   Thu May 23 10:49:19 2024 +0200
    
        fix(ready.js): upload command

--- src/Interactions/ApplicationCommands/imagine.js
+++ src/Interactions/ApplicationCommands/imagine.js
@@ -15,9 +15,11 @@ export default {
 
         const userPrompt = interaction.options.getString("description")
 
+        const botName = `${interaction.client?.user?.displayName || "Bot"}`;
+
         if(!userPrompt) return interaction.reply({ content : "Please provide a prompt", ephemeral : true })
 
-        await interaction.reply({ content : `${interaction.client?.user?.displayName || "Bot"} is checking your daily quota...`,ephemeral : true})
+        await interaction.reply({ content : `${botName} is checking your daily quota...`,ephemeral : true})
 
         var user = new User(interaction.user.id, interaction.member._roles)
 
@@ -36,40 +38,40 @@ export default {
             var firstTime = sorted[0].timestamp
             var nextTime = firstTime + 24 * 60 * 60 * 1000
 
-            return interaction.editReply({ content : `You have reached your daliy limit as ${usage.length} out of ${quota} images have been generated. You can generate more images after <t:${parseInt(nextTime / 1000)}>`, ephemeral : true })
+            return interaction.editReply({ content : `You have reached your daliy limit as ${usage.length} out of ${quota} images have been imagined.\n${botName} can imagine more images for you after <t:${parseInt(nextTime / 1000)}>`, ephemeral : true })
 
         }
 
         var doc = await user.createDocument(userPrompt)
 
-        if(!doc) return interaction.editReply({ content : `The bot is a little confused, we would appreciate it if you let us know the error code!\n\nError Code: 0x1`})
+        if(!doc) return interaction.editReply({ content : `${botName} is a little confused, contact support with the error code!\n\nError Code: 0x1`})
 
-        await interaction.editReply({ content : `${interaction.client?.user?.displayName || "Bot"} is thinking about **${userPrompt}**...`, ephemeral : true})
+        await interaction.editReply({ content : `${botName} is thinking about **${userPrompt}**...`, ephemeral : true})
 
         const image = await Client.imagineDragon(userPrompt, interaction.user.id)
 
         if(image instanceof Error){
             await user.deleteDocument(doc).then(() => {
-                return interaction.editReply({ content : `Someone is filling your daily quota! Let us know immediately!\n\nError Code : 0x2\n_id${doc._id}`})
+                return interaction.editReply({ content : `Someone is filling your daily quota! Contact the support immediately!\n\nError Code : 0x2\n_id${doc._id}`})
             })
             if(image.code == 400){
-                return interaction.editReply({ content : `The bot thinks this prompt is harmful. Please try another prompt.\n\nPrompt : ${userPrompt}`, ephemeral : true})
+                return interaction.editReply({ content : `${botName} thinks this prompt is harmful. Please try another prompt.\n\nPrompt : ${userPrompt}`, ephemeral : true})
             }else if(image.code == 500){
-                return interaction.editReply({ content : `The bot is on vacation. Please try again later.`, ephemeral : true})
+                return interaction.editReply({ content : `${botName} is on vacation. Please try again later.`, ephemeral : true})
             }else if(image.code == 429){
-                return interaction.editReply({ content : `The bot is a little tired. Please try again later.`, ephemeral : true})
+                return interaction.editReply({ content : `${botName} is a little tired. Please try again later.`, ephemeral : true})
             }else if(image.code == 501){
-                return interaction.editReply({ content : `The bot is a little confused. Please try again later.`, ephemeral : true})
+                return interaction.editReply({ content : `${botName} is a little confused. Please try again later.`, ephemeral : true})
             }
         }
 
 
-        return await interaction.channel.send({ content : `${interaction.user}'s Puff Dragon!`, files : [image.buffer] }).then(async (message) => {
-            await interaction.editReply({ content : `${interaction.client?.user?.displayName || "Bot"} successfully imagined ${userPrompt}.\nDescription : ${userPrompt}\nYour Quota (Daily) = ${usage.length+1}/${quota} `, ephemeral : true })
+        return await interaction.channel.send({ content : `Here is ${interaction.user}'s Dragon!`, files : [image.buffer] }).then(async (message) => {
+            await interaction.editReply({ content : `${botName} successfully imagined **${userPrompt}**.\n\n📜 Daily quota - ${usage.length+1} / ${quota} `, ephemeral : true })
             await user.updateDocument(doc, message.id, userPrompt, image.revisedPrompt, message.attachments?.first()?.url || null)
         }).catch(async err => {
             if(process.env.DEBUG) console.error(err)
-            await interaction.editReply({ content : `Bot wants to show this perfect picture he made, just to you!`, ephemeral : true, files : [image.buffer]})
+            await interaction.editReply({ content : `${botName} is showing his artwork just to you!`, ephemeral : true, files : [image.buffer]})
         })
         
 
