f28feea4d5f5d15e20a28d629b79525fcfbe8ece
0xEzSwim <enzo.chantereault@efrei.net>
2024-06-04 22:14:22 +0200

    feat(roles): update roles limit 2

--- src/Models/User/Config.js
+++ src/Models/User/Config.js
@@ -1,7 +1,7 @@
 export const DailyLimits = {
-    "1233330323878711367" : 6, // Legendary Dragon
-    "1233330005573107712" : 6, // Black Dragon
-    "1233329924929097758" : 6, // Fearie Dragon
+    "1233330323878711367" : 9, // Legendary Dragon
+    "1233330005573107712" : 9, // Black Dragon
+    "1233329924929097758" : 9, // Fearie Dragon
     "1233329868104794122" : 6, // Wyvern
     "1223258152166752326" : 3, // Dragon Hatchling
     "1242908037958734012" : 4  // Test
